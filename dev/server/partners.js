f9e0c2338c7e8200fecc7a82492b3b0833f926db
jeffw62 <kozakvod@gmail.com>
2025-11-11 07:33:06 -0600

    Create partners.js

---
+++ dev/server/partners.js
@@ -0,0 +1,9 @@
+// =======================================================
+// ConciergeSync™ Partner Bridge
+// Unified import/export hub for all provider integrations
+// =======================================================
+
+export { SeatsAeroService, applySanityFilter } from "./seatsaero.js";
+export { fetchCashFare } from "./serpapi.js";
+export * as Duffel from "./duffel.js";
+export * as Plaid from "./plaid.js";
