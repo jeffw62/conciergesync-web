2abecbaa7f20f84f1673fb597f9e184b00f32d22
Jeff Wynn <kozakvod@gmail.com>
2026-01-07 15:32:34 -0600

    Update plaid.routes.js

--- dev/server/plaid.routes.js
+++ dev/server/plaid.routes.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
 import { createLinkToken, exchangePublicToken, firestoreTest } from "./plaid.js";
-import admin from "firebase-admin";
+import admin from "./dev/server/firebase.js";
 
 if (!admin.apps.length) {
   admin.initializeApp({
