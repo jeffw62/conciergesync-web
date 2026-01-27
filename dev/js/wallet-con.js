126cb49d4ae2a723bf785d1f09553610e110e1c6
Jeff Wynn <kozakvod@gmail.com>
2026-01-27 05:10:15 -0600

    Create wallet-con.js

---
+++ dev/js/wallet-con.js
@@ -0,0 +1,11 @@
+const walletCards = document.querySelectorAll('.wallet-card');
+
+walletCards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.classList.add('is-hovered');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.classList.remove('is-hovered');
+  });
+});
